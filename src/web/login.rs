b262ee82ec07e61eaf10cebdaa7044e1d1c7e1b6
oleg.medovikov <oleg.medovikov@mail.ru>
2025-03-25 08:09:02 +0000

    add login

--- src/web/login.rs
+++ src/web/login.rs
@@ -61,42 +61,42 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 #[get("/loginCheck.js")]
 pub fn login_check_js() -> RawJavaScript<&'static str> {
     let content = r#"
-        // Проверяем наличие токена в localStorage
-        const authToken = localStorage.getItem('authToken');
+// Проверяем наличие токена в localStorage
+const authToken = localStorage.getItem('authToken');
 
-        if (!authToken) {
-          // Если токена нет, перенаправляем на страницу входа
-          window.location.href = '/login';
-        } else {
-          // Отправляем запрос к API для проверки токена
-          fetch('/api/check_token', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-              token: authToken
-            })
-          })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Ответ сети был неудовлетворительным:');
-            }
-            return response.json();
-            window.location.href = '/login';
-          })
-          .then(data => {
-            if (!data.token_valid) {
-              // Если токен не валиден, перенаправляем на страницу входа
-              window.location.href = '/login';
-            }
-          })
-          .catch(error => {
-            console.error('Возникла проблема с операцией выборки:', error);
-            // В случае ошибки также перенаправляем на страницу входа
-            window.location.href = '/login';
-          });
-        }
+if (!authToken) {
+  // Если токена нет, перенаправляем на страницу входа
+  window.location.href = '/login';
+} else {
+  // Отправляем запрос к API для проверки токена
+  fetch('/api/check_token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      token: authToken
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Ответ сети был неудовлетворительным:');
+    }
+    return response.json();
+    window.location.href = '/login';
+  })
+  .then(data => {
+    if (!data.token_valid) {
+      // Если токен не валиден, перенаправляем на страницу входа
+      window.location.href = '/login';
+    }
+  })
+  .catch(error => {
+    console.error('Возникла проблема с операцией выборки:', error);
+    // В случае ошибки также перенаправляем на страницу входа
+    window.location.href = '/login';
+  });
+}
     "#;
     RawJavaScript(content)
 }
